286c02111f667f81f4f65450a5965fb18c941c1e
Vuong Vu <vuong_vu@Vuongs-MacBook-Pro.local>
2020-01-10 12:38:47 +0100

    Updates

---
+++ precache-manifest.087919ea091e54e93315463d1c391c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae760be347e816f348a8eed7c041cac9",
+    "url": "/cats/index.html"
+  },
+  {
+    "revision": "a8655d32bbbef8ca6140",
+    "url": "/cats/static/css/main.9c57ef3c.chunk.css"
+  },
+  {
+    "revision": "dd2fa113a202e7c66570",
+    "url": "/cats/static/js/2.7b2a9693.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cats/static/js/2.7b2a9693.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8655d32bbbef8ca6140",
+    "url": "/cats/static/js/main.9adc446d.chunk.js"
+  },
+  {
+    "revision": "04661e4fddf6f43135fa",
+    "url": "/cats/static/js/runtime-main.6847a78e.js"
+  }
+]);
